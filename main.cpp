e4a027b7e0b29c199648cdcc9b5eb8c59a4459eb
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-11-21 17:37:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int a=1;
+    int b=1;
+    int c;
+    int n;
+    cin>>n;
+    if(n>=2)
+    {
+      for (int i=0; i<n; i++){
+      cout<<a<<endl;
+      c=a;
+      a=b;
+      b=a+c;
+    }
+    }
+    else
+    cout<<"errore";
+    
+    return 0;
 }
